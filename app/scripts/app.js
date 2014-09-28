9acaa2cbce71cffc842e9be54f32f4bad0193d99
Juan Orozco <juanthedesigner@gmail.com>
2014-09-28 01:10:31 -0400

    Improving view.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,17 +9,31 @@ window.websiteModel = Websites;
     tag:'websites-list',
     template:can.view('website-list'),
     scope:{
-      sites: new can.List([])
+      sites: new can.List([]),
+      toggle_visibility:function($el){
+        console.log($el);
+        var state = $el.parent('li').find('.item-content').is(":visible");
+        if (state){
+          $el.parent('li').find('.item-content').hide();
+        }else{
+          $el.parent('li').find('.item-content').show();
+        }
+      }
     },
     events:{
       inserted:function(){
         var scope = this.scope;
         Websites.findAll().then(function(resp){
           scope.attr('sites',resp);
+          $('.toggle-site-visibility h2').each(scope.toggle_visibility);
         })
         .fail(function(a,b,c){
           alert(a);console.log(a);console.log(b);console.log(c)
         });
+
+      },
+      '.toggle-site-visibility click':function($el){
+        this.scope.toggle_visibility($el);
       }
     }
   });
