b7cbf25ba3c08f6590a3b161e7058bc123968137
Juan Orozco <juanthedesigner@gmail.com>
2014-09-28 00:24:00 -0400

    Add components and templates.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,34 @@
-//load json
-//var websites = can.model.extend();
-console.log("App ready.");
+var app = function(){
+  //load json
+  var Websites = can.Model.extend({
+      findAll: 'GET /websites.json'
+    },{});
+window.websiteModel = Websites;
+  //app component
+  var websiteList = can.Component.extend({
+    tag:'websites-list',
+    template:can.view('website-list'),
+    scope:{
+      sites: new can.List([])
+    },
+    events:{
+      inserted:function(){
+        var scope = this.scope;
+        Websites.findAll().then(function(resp){
+          scope.attr('sites',resp);
+        })
+        .fail(function(a,b,c){
+          alert(a);console.log(a);console.log(b);console.log(c)
+        });
+      }
+    }
+  });
+
+  //new websiteList('#app');
+  $('#app').html(can.view.mustache('<websites-list></websites-list>' ));
+
+  console.log("App ready!");
+  return can;
+}
+
+new app();
